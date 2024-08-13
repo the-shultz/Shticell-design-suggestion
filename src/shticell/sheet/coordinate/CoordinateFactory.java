c8badb07f6bb4fe692741e26c364fce9b0ade09d
Aviad Cohen <aviadc@liveperson.com>
2024-08-13 19:40:53 +0300

    coordinate factory. Take 2

--- src/shticell/sheet/coordinate/CoordinateFactory.java
+++ src/shticell/sheet/coordinate/CoordinateFactory.java
@@ -1,8 +1,22 @@
 package shticell.sheet.coordinate;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class CoordinateFactory {
 
+    private static Map<String, Coordinate> cachedCoordinates = new HashMap<>();
+
     public static Coordinate createCoordinate(int row, int column) {
-        return new CoordinateImpl(row, column);
+
+        String key = row + ":" + column;
+        if (cachedCoordinates.containsKey(key)) {
+            return cachedCoordinates.get(key);
+        }
+
+        CoordinateImpl coordinate = new CoordinateImpl(row, column);
+        cachedCoordinates.put(key, coordinate);
+
+        return coordinate;
     }
 }
